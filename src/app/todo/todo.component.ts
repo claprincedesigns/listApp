2bffe999ea6539c19db52d7999fc6bc72feb7122
Courtney La Prince <courtney.la.prince@ibm.com>
2018-04-04 12:36:16 -0400

    List component updated app working

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,9 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
+  title = 'To-Do List';
+  newTodo: string;
+  todos = [
+    {id: 0},
+    {name: 'string'}
+  ];
+
 
   constructor() {
+    this.todos = [];
+    this.newTodo = '';
+  }
 
+  addTodo() {
+    this.todos.push(this.newTodo);
+    this.newTodo = '';
+  }
+  
+  deleteTodo(name) {
+    for(let i = 0; i < this.todos.length; i++) {
+      if(this.todos[i]["name"] == name){
+        this.todos.splice(i, 1);
+      }
+    }
+
+     // this.todos = this.todos.filter(todo => todo.id !== id);
+     // this.todos.splice(id, 1);
   }
 
   ngOnInit() {
