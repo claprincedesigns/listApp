7d7cee917016b1dbd531ef9b30a90379ff9bbf3a
Courtney La Prince <courtney.la.prince@ibm.com>
2018-04-05 17:08:36 -0400

    Updated specs for testing

--- src/app/todo/todo.component.spec.ts
+++ src/app/todo/todo.component.spec.ts
@@ -1,14 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoComponent } from './todo.component';
+import { FormsModule } from '@angular/forms';
 
 describe('TodoComponent', () => {
   let component: TodoComponent;
   let fixture: ComponentFixture<TodoComponent>;
+  let compiled;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodoComponent ]
+      declarations: [ TodoComponent ],
+      imports: [FormsModule]
     })
     .compileComponents();
   }));
@@ -17,9 +20,25 @@ describe('TodoComponent', () => {
     fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render title in a h1 tag', async(() => {
+    expect(compiled.querySelector('h1').textContent).toEqual('To-Do List');
+  }));
+
+  it('should display the count of todos', async(() => {
+    component.todoCount = 5;
+    fixture.detectChanges();
+
+    expect(compiled.querySelector('.header p').textContent).toContain('5');
+  }));
+
+  it('should display an item in list for each todos', () => {
+    component.todos = ['Rubbing Lotion', 'Basketball', 'Cake'];
+    fixture.detectChanges();
+
+    expect(compiled.querySelectorAll('li').length).toEqual(3);
+    expect(compiled.querySelectorAll('li')[0].textContent).toContain('Rubbing Lotion');
   });
 });
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
+  idCounter = 1;
   title = 'To-Do List';
   todoCount: number;
   newTodo: string;
-  todos = ['Milk'];
+  todos = [{
+    id: 0,
+    name: 'milk'
+  }];
 
   constructor() { }
 
@@ -18,14 +22,32 @@ export class TodoComponent implements OnInit {
   }
 
   addTodo() {
-    this.todos.push(this.newTodo);
+    const todo = {
+      id: this.idCounter,
+      name: this.newTodo
+    }
+    this.todos.push(todo);
+    
+
     this.newTodo = '';
     this.todoCount = this.todos.length;
+    this.idCounter++;
   }
   
   deleteTodo(i) {
       this.todos.splice(i, 1);
-    }
+  }
+
+  updateTodo(i) {
+    // add an click to edit or icon(bonus)
+    // flag isEditing boolean to check the state of the item
+    // if isEditing is true show input box; if false don't show it
+    // 
+    // shouldn't save if the string is empty
+    // can cancel edits
+    // and test
+    //create a new component for todoItem
+  }
 
      // this.todos = this.todos.filter(todo => todo.id !== id);
      
